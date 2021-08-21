1aaf0847df8ab732336499dcd5df6399931f848f
Alex White <alexlloydwhite@gmail.com>
2021-08-21 12:22:57 -0500

    adding numbers test

--- sum.test.js
+++ sum.test.js
@@ -24,3 +24,21 @@ describe("truthy or falsy", () => {
     expect(n).toBeNull();
   });
 });
+
+describe("numbers", () => {
+  it("two plus two", () => {
+    const value = 2 + 2;
+    expect(value).toBe(4);
+    expect(value).toBeGreaterThan(3);
+    expect(value).toBeGreaterThanOrEqual(4);
+    expect(value).toBeLessThan(7);
+    expect(value).toBeLessThanOrEqual(4);
+  });
+
+  it("adding floats", () => {
+    const value = 0.1 + 0.2;
+    expect(value).toBeCloseTo(0.3);
+  });
+});
+
+
