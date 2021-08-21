e33dd67234e1a985d35c5979dc39846b2f7d9bb2
Alex White <alexlloydwhite@gmail.com>
2021-08-21 10:23:11 -0500

    creating truthy falsy test

--- sum.test.js
+++ sum.test.js
@@ -1,6 +1,26 @@
 const sum = require("./sum");
 
-it("Should add two numbers and return results", () => {
-    const result = sum(1,2);
+describe("Example test", () => {
+  it("Should add two numbers and return results", () => {
+    const result = sum(1, 2);
     expect(result).toBe(3);
+  });
+
+  // empty object or array will
+  // return false on {} === {}
+  // use toEqual instead of toBe
+  it("object assignment", () => {
+    const obj = {};
+    expect(obj).toEqual({});
+  });
+});
+
+describe("truthy or falsy", () => {
+  it("null", () => {
+    const n = null;
+    expect(n).toBeFalsy();
+    // not will give opposite here
+    expect(n).not.toBeTruthy();
+    expect(n).toBeNull();
+  });
 });
