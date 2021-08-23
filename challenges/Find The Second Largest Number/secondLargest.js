a588a98580a4c0968f8a0618cbf3ca6dc5304aa4
Alex White <alexlloydwhite@gmail.com>
2021-08-23 15:19:17 -0500

    all tests passing

--- challenges/Find The Second Largest Number/secondLargest.js
+++ challenges/Find The Second Largest Number/secondLargest.js
@@ -1,5 +1,20 @@
+// Insertion sort
 const secondLargest = (array) => {
-    return;
-}
+  for (let i = 0; i < array.length; i++) {
+    // First element in unsorted array
+    let current = array[i];
+    // Last element in unsorted array
+    let j = i - 1;
+    while (j > -1 && current < array[j]) {
+      array[j + 1] = array[j];
+      j--;
+    }
+    array[j + 1] = current;
+  }
+  return array[array.length - 2];
+};
+
+// Optimized Solution
+// const secondLargest = a => a.sort((a,b)=> b - a)[1];
 
 module.exports = secondLargest;
