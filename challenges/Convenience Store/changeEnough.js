ab04b53f77deb97c0379e5452b57bb40bbd90773
Alex White <alexlloydwhite@gmail.com>
2021-08-23 14:40:46 -0500

    simplifing fn

--- challenges/Convenience Store/changeEnough.js
+++ challenges/Convenience Store/changeEnough.js
@@ -1,17 +1,8 @@
 const changeEnough = (change, amountDue) => {
-  let quarters = change[0] * 0.25;
-  let dimes = change[1] * 0.1;
-  let nickels = change[2] * 0.05;
-  let pennies = change[3] * 0.01;
+  let totalChange =
+    change[0] * 0.25 + change[1] * 0.1 + change[2] * 0.05 + change[3] * 0.01;
 
-  let totalChange = quarters + dimes + nickels + pennies;
-
-  if (totalChange >= amountDue) {
-    return true;
-  } else {
-    return false;
-  }
-  
+  return totalChange >= amountDue;
 };
 
 module.exports = changeEnough;
