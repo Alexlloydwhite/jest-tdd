fee68f0dfb542fd69048831de20c0c72ba77b032
Alex White <alexlloydwhite@gmail.com>
2021-08-23 14:38:46 -0500

    all tests passing

--- challenges/Convenience Store/changeEnough.js
+++ challenges/Convenience Store/changeEnough.js
@@ -1,5 +1,17 @@
 const changeEnough = (change, amountDue) => {
-    return;
-}
+  let quarters = change[0] * 0.25;
+  let dimes = change[1] * 0.1;
+  let nickels = change[2] * 0.05;
+  let pennies = change[3] * 0.01;
+
+  let totalChange = quarters + dimes + nickels + pennies;
+
+  if (totalChange >= amountDue) {
+    return true;
+  } else {
+    return false;
+  }
+  
+};
 
 module.exports = changeEnough;
