0b0825085ebbc392f20ad58a7a62331efd819787
Alex White <alexlloydwhite@gmail.com>
2021-08-23 12:06:24 -0500

    optimizing checkTrue function

--- challenges/How Much Is True?/true.js
+++ challenges/How Much Is True?/true.js
@@ -1,10 +1,6 @@
 const checkTrue = (array) => {
   let trueCount = 0;
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] === true) {
-      trueCount++;
-    }
-  }
+  array.map((index) => index === true && trueCount++);
   return trueCount;
 };
 
