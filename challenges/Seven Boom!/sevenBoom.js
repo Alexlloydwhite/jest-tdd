fd722eccf526032cb4cd02c77c9769247a9b4c2c
Alex White <alexlloydwhite@gmail.com>
2021-08-23 13:40:07 -0500

    passes tests

--- challenges/Seven Boom!/sevenBoom.js
+++ challenges/Seven Boom!/sevenBoom.js
@@ -1,5 +1,13 @@
 const sevenBoom = (array) => {
-  return;
+  let stringArray = array.toString();
+  for (let i = 0; i < stringArray.length; i++) {
+    for (let j = 0; j < stringArray[i].length; j++) {
+      if (stringArray[i][j] === "7") {
+        return "Boom!";
+      }
+    }
+  }
+  return "there is no 7 in the array";
 };
 
 module.exports = sevenBoom;
