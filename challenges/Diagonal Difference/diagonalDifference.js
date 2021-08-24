fafde23506049446341d26fd52fb160bb17dba87
Alex White <alexlloydwhite@gmail.com>
2021-08-24 13:39:20 -0500

    tests are passing

--- challenges/Diagonal Difference/diagonalDifference.js
+++ challenges/Diagonal Difference/diagonalDifference.js
@@ -6,7 +6,15 @@
  */
 
 function diagonalDifference(array) {
-  return 0;
+  let diagonal1Total = 0;
+  let diagonal2Total = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    diagonal1Total += array[i][i];
+    diagonal2Total += array[i][array.length - 1 - i];
+  }
+
+  return Math.abs(diagonal1Total - diagonal2Total);
 }
 
 module.exports = diagonalDifference;
