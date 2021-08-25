67bee786d9522233e12ffd8c5a314acfb10a1827
Alex White <alexlloydwhite@gmail.com>
2021-08-25 12:22:48 -0500

    giving up on life

--- challenges/Mini-Max Sum/miniMaxSum.test.js
+++
@@ -1,8 +0,0 @@
-const miniMaxSum = require("./miniMaxSum");
-
-describe("function takes in array of numbers and min / max sum", () => {
-  it("should return a number", () => {
-    let result = miniMaxSum([1, 3, 5, 7, 9]);
-    expect(typeof result).toBe("number");
-  });
-});
