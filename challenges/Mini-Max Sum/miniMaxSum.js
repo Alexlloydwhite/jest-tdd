99fe7c205ffcf7adc90aa9bec7c52bd3b54e2c12
Alex White <alexlloydwhite@gmail.com>
2021-08-25 12:08:29 -0500

    setting up fn and test

---
+++ challenges/Mini-Max Sum/miniMaxSum.js
@@ -0,0 +1,12 @@
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function miniMaxSum(array) {
+  let result = 0
+  return result;
+}
+
+module.exports = miniMaxSum;
